b88c3b4c34ff4602ea1751b331916bd708ba34dc
Yohan Giarelli <yohan@les-tilleuls.coop>
2025-10-07 21:52:11 +0000

    tests: Added some e2e tests

---
+++ tests/e2e/consent-dialog.spec.ts
@@ -0,0 +1,35 @@
+import { test, expect, describe } from '@playwright/test';
+
+describe('Consent dialog box', () => {
+    test('It displays consent dialog box', async ({page}) => {
+        await page.goto('https://www.localhost');
+        await page.evaluate(() => document.querySelector('.sf-toolbar').style.display = 'none');
+
+        await expect(page.locator('#consent-dialog')).toHaveScreenshot();
+    });
+
+    test('It checks trackers manually', async ({page}) => {
+        await page.goto('https://www.localhost');
+        await page.evaluate(() => document.querySelector('.sf-toolbar').style.display = 'none');
+
+        await page.locator('.Dialog__tracker:nth-child(1) label').click();
+        await page.locator('.Dialog__tracker:nth-child(2) label').click();
+
+        await expect(page.locator('#consent-dialog')).toHaveScreenshot();
+    });
+
+    test('It checks trackers when accepting all', async ({page}) => {
+        await page.goto('https://www.localhost');
+        await page.evaluate(() => document.querySelector('.sf-toolbar').style.display = 'none');
+
+        await page.locator('#accept-all').click();
+        await page.waitForLoadState('networkidle');
+        await expect(page.locator('#show-consent-dialog')).toHaveScreenshot();
+
+        await page.reload({waitUntil: 'networkidle'});
+        await page.evaluate(() => document.querySelector('.sf-toolbar').style.display = 'none');
+
+        await page.locator('#show-consent-dialog').click();
+        await expect(page.locator('#consent-dialog')).toHaveScreenshot();
+    });
+});
